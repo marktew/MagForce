17cca6fff3c4948516f86c88c5adf8eb31d1272a
Mark Tew <mark@weexcel.co.za>
2015-11-26 05:09:15 -0500

    Init

---
+++ Block/MagForce.php
@@ -0,0 +1,6 @@
+namespace MarkTew\MagForce\Block;
+class Hello extends \Magento\Framework\View\Element\Template {
+    public function _prepareLayout() {
+        return parent::_prepareLayout();
+    }
+}
---
+++ Controller/Index/Index.php
@@ -0,0 +1,8 @@
+namespace MarkTew\MagForce\Controller\Index;
+class Index extends \Magento\Framework\App\Action\Action {
+    public function execute() {
+        $this->_view->loadLayout();
+        $this->_view->getLayout()->initMessages();
+        $this->_view->renderLayout();
+    }
+}
